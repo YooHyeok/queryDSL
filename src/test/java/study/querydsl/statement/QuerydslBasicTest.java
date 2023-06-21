b686965c4ac904e657b3af91ed812479a7353227
YooHyeok <yjk7454@naver.com>
2023-06-21 12:22:17 +0900

    벌크연산 (수정/삭제 및 1차캐시 우선권 개념)

--- src/test/java/study/querydsl/statement/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/statement/QuerydslBasicTest.java
@@ -846,5 +846,80 @@ public class QuerydslBasicTest {
     private BooleanExpression allOrEq(String usernameCond, Integer ageCond) {
         return usernameEq(usernameCond).or(ageEq(ageCond));
     }
+
+    /**
+     * 벌크연산(update)
+     */
+    @Test
+    @Commit
+    public void bulkUpdate() {
+        long count = queryFactory
+                .update(member)
+                .set(member.username, "비회원")
+                .where(member.age.lt(28)) //28살 미만 회원에 대해서 이름을 비회원으로 변경
+                .execute();
+        em.clear(); // 영속성 1차캐시 초기화
+        /**
+         * [영속성 컨텍스트]
+         * Member1 = 10 -> Member1
+         * Member2 = 20 -> Member2
+         * Member3 = 30 -> Member3
+         * Member4 = 40 -> Member4
+         *
+         * [bulk연산 - DB] : 영속성컨텍스트를 건너뛰고 DB에 바로 퀴리 반영
+         * Member1 = 10 -> 비회원
+         * Member2 = 20 -> 비회원
+         * Member3 = 30 -> Member3
+         * Member4 = 40 -> Member4
+         *
+         * Execute - 반영할때는 영속성컨텍스트를 무시하고 DB에 반영해 버리지만,
+         * Select - 불러올때는 영속성컨텍스트가 항상 우선권을 가진다.
+         * 다시 말해 조회해온 결과를 캐시에 엎어 쓰지 않는다.
+         * 이것은 JPQL에서도 동일하게 적용된다.
+         * 따라서 벌크연산 후에는 반드시 1차캐시를 초기화 해주는게 좋다.
+         */
+        List<Member> result = queryFactory.selectFrom(member).fetch();
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
+    }
+
+    /**
+     * 벌크연산(update)
+     * add(덧,뺄셈), multiply(곱하기)
+     */
+    @Test
+    public void bulkAdd() {
+        long execute = queryFactory
+                .update(member)
+//                .set(member.age, member.age.add(1)) // 모든 회원의 나이를 1씩 더한다.
+//                .set(member.age, member.age.add(-1)) // 빼기의 경우 정수 앞에 - 를 추가하면 된다
+                .set(member.age, member.age.multiply(-1)) // 곱하기
+                .execute();
+
+        em.clear(); //1차캐시 초기화
+
+        System.out.println("execute = " + execute);
+        List<Member> result = queryFactory.selectFrom(member).fetch();
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
+    }
+
+    /**
+     * 벌크연산 (삭제)
+     */
+    @Test
+    public void bulkDelete() {
+        long count = queryFactory
+                .delete(member)
+                .where(member.age.gt(18))
+                .execute();
+        em.clear();
+        List<Member> result = queryFactory.selectFrom(member).fetch();
+        for (Member member1 : result) {
+            System.out.println("member1 = " + member1);
+        }
+    }
 }
 
