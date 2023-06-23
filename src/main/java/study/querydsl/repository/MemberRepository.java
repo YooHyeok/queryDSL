a5cac4e31de8cab0b2f5bde251924fda43d0688d
YooHyeok <yjk7454@naver.com>
2023-06-23 23:14:13 +0900

    QuerydslPredicateExecutor - 구현 및 테스트

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
@@ -9,10 +10,17 @@ import java.util.List;
  * SpringDataJpaRepository <br/>
  * 스프링 데이터 JPA에서 QueryDsl을 적용하기 위한 repository <br/>
  * QueryDsl을 구현한 구현체로 implementation한 대상 인터페이스를 상속받는다. (MemberRepositoryCustom) <br/>
- * 구현체인 MemberRepositoryCustom을 implementation할 클래스를 생성한 뒤 메소드를 Override하여 구현한다.
- *
+ * 구현체인 MemberRepositoryCustom을 implementation할 클래스를 생성한 뒤 메소드를 Override하여 구현한다. <br/>
+ * <br/>
+ * QuerydslPredicateExecutor 기능 추가 <br/>
+ * 스프링데이터JPA 기본 지원 쿼리메소드에 QueryDsl문법을 부여하여 검색조건을 만들 수 있다. <br/>
+ * (Pageable, Sort 모두 지원하고, 정상적으로 동작한다.)<br/>
+ * ***한계점*** <br/>
+ * -> JOIN이 되지 않는다.(묵시적 조인은 가능하지만 Left Join 불가능) <br/>
+ * -> 클라이언트(Repository)가 Querydsl에 의존 -> 서비스 클래스가 Querydsl이라는 구현기술에 의존해야 한다. <br/>
+ * -> 복잡한 실무 환경에서 사용하기에는 한계가 명확하다.(실무 권장 X)
  */
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom{
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
 
     /**
      * SpringDataJPA 쿼리메소드 <br/>
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -98,6 +99,28 @@ class MemberRepositoryTest {
         Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("Member1","Member2","Member3");
+    }
 
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("Member1", 10, teamA);
+        Member member2 = new Member("Member2", 20, teamA);
+        Member member3 = new Member("Member3", 30, teamB);
+        Member member4 = new Member("Member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        QMember member = QMember.member;
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("Member1")));
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
     }
 }
