0a820a98a91091b1a313bb492a7104da348092db
YooHyeok <yjk7454@naver.com>
2023-06-23 21:35:29 +0900

    스프링 데이터 queryDsl 페이징구현 및 테스트 - findResult통합/find,findcount분리

--- src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryCustomImpl.java
@@ -1,7 +1,11 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.StringUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -38,11 +42,85 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                 .leftJoin(member.team, team)
                 .where(usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
-//                        ageGoe(condition.getAgeGoe()),
-//                        ageLoe(condition.getAgeLoe())
                         ageBetween(condition.getAgeGoe(),condition.getAgeLoe())
                 )
                 .fetch();
+    }
+
+    /**
+     * [queryDsl - 페이징] content, count 통합 <br/>
+     * 데이터가 별로 없을경우 사용 <br/>
+     * fetchResults()에 의해 content와 count 쿼리를 동시에 날림 <br/>
+     * result객체로 부터 content와 count를 추출하여 Page인터페이스 구현 객체에 담아 return.
+     * @param condition
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> pageResult = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageBetween(condition.getAgeGoe(), condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();//Conten용쿼리 Count용쿼리 각각 2번 출력
+        List<MemberTeamDto> content = pageResult.getResults();
+        long total = pageResult.getTotal();
+        return new PageImpl<>(content, pageable, total); //content, pageable, total 정보 page구현체 객체에 담아 모두반환
+    }
+
+    /**
+     * [queryDsl - 페이징] content, count 분리 (몇천만건의 조회 최적화)<br/>
+     * Count쿼리의 경우 특정 상황에는 Join이 필요하지 않을 수 있다. <br/>
+     * 1) 심플화 되어있거나, <br/>
+     * 2) CountData가 Db에 저장되어있거나, <br/>
+     * 3) join을 줄이더라도 content쿼리와 count쿼리가 차이가 없거나 <br/>
+     * 4) count 쿼리를 먼저 실행 했는데 카운트가 발생하지 않을 경우 content조회를 하지 않는다거나
+     * @param condition
+     * @param pageable
+     * @return
+     */
+    @Override
+    public Page<MemberTeamDto> searchComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageBetween(condition.getAgeGoe(), condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();//Conten용쿼리
+
+        long total = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageBetween(condition.getAgeGoe(), condition.getAgeLoe())
+                ).fetchCount(); //Count용쿼리
+
+        return new PageImpl<>(content, pageable, total); //content, pageable, total 정보 page구현체 객체에 담아 모두반환
     }
 
     /**
--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -3,6 +3,8 @@ package study.querydsl.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
@@ -72,6 +74,29 @@ class MemberRepositoryTest {
         condition.setTeamName("teamB");
         List<MemberTeamDto> result = memberRepository.search(condition);
         assertThat(result).extracting("username").containsExactly("Member3","Member4");
+
+    }
+    @Test
+    public void  searchPageSimple() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("Member1", 10, teamA);
+        Member member2 = new Member("Member2", 20, teamA);
+        Member member3 = new Member("Member3", 30, teamB);
+        Member member4 = new Member("Member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+
+        PageRequest pageRequest = PageRequest.of(0, 3); //page가 0이면 query상에 offset 절이 추가되지 않는다.
+        Page<MemberTeamDto> result = memberRepository.searchPageSimple(condition, pageRequest);
+        assertThat(result.getSize()).isEqualTo(3);
+        assertThat(result.getContent()).extracting("username").containsExactly("Member1","Member2","Member3");
 
     }
 }
